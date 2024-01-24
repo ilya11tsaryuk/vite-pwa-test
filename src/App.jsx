8ca7d833752a7a396da025d2c6c06f2f2deaa118
ilya11tsaryuk <ilya11tsaryuk@gmail.com>
2024-01-24 17:37:53 +0300

    return changed

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          было {count} кликов
+          count {count}
         </button>
       </div>
     </>
